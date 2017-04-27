d186aca008343d5023d7cb40a7ebc47704a75fa5
Земцева Виктория <jlgfmhldf@gmail.com>
2017-04-27 10:23:51 +0300

    fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -4,11 +4,9 @@ const root = path.join(__dirname, './app')
 
 /* eslint-disable global-require */
 module.exports = webpack => ({
-    plugins: function () {
-        return [
+    plugins: [
             require('precss'),
             require('postcss-custom-properties'),
             require('autoprefixer'),
-        ];
-    }
+        ]
 })
