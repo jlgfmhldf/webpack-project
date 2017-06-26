089521bbb14d1b313400931e58d216792d272f3b
Земцева Виктория <jlgfmhldf@gmail.com>
2017-06-26 23:26:48 +0300

    prototype

--- app/containers/App/App.jsx
+++ app/containers/App/App.jsx
@@ -1,6 +1,18 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { Button } from 'react-toolbox/lib/button'
+import {
+	Layout,
+	Panel,
+	AppBar,
+	Checkbox,
+	Card,
+	CardMedia,
+	CardTitle,
+	CardText,
+	CardActions,
+} from 'react-toolbox'
+
 
 const mapStateToProps = state => ({
 	test: state.test,
@@ -15,9 +27,204 @@ export default class App extends PureComponent {
 	render() {
 		return (
 			<div>
-				<div>
-					<Button label='Hello!!' raised primary/>
-				</div>
+				<Layout>
+					<Panel>
+						<AppBar leftIcon='menu' />
+
+						<p>
+							Выберите продукты, которые есть у вас в холодильнике:
+						</p>
+						<Checkbox label="Бананы"/>
+						<Checkbox label="Малина"/>
+						<Checkbox label="Клубника"/>
+						<Checkbox label="Бананы"/>
+
+						<Button label='Далее' raised primary/>
+						<h2>
+							Результаты поиска:
+						</h2>
+						<div style={{ width: '25%'}}>
+							<Card>
+								<CardTitle
+									avatar="https://placeimg.com/80/80/animals"
+									title="Avatar style title"
+									subtitle="Subtitle here"
+								/>
+								<CardMedia
+									aspectRatio="wide"
+									image="https://placeimg.com/800/450/nature"
+								/>
+								<CardTitle
+									title="Title goes here"
+									subtitle="Subtitle here"
+								/>
+								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+								<CardActions >
+									<Button label="Action 1" />
+									<Button label="Action 2" />
+								</CardActions>
+							</Card>
+						</div>
+
+						<div style={{ width: '25%'}}>
+							<Card>
+								<CardTitle
+									avatar="https://placeimg.com/80/80/animals"
+									title="Avatar style title"
+									subtitle="Subtitle here"
+								/>
+								<CardMedia
+									aspectRatio="wide"
+									image="https://placeimg.com/800/450/nature"
+								/>
+								<CardTitle
+									title="Title goes here"
+									subtitle="Subtitle here"
+								/>
+								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+								<CardActions >
+									<Button label="Action 1" />
+									<Button label="Action 2" />
+								</CardActions>
+							</Card>
+						</div>
+
+						<div style={{ width: '25%'}}>
+							<Card>
+								<CardTitle
+									avatar="https://placeimg.com/80/80/animals"
+									title="Avatar style title"
+									subtitle="Subtitle here"
+								/>
+								<CardMedia
+									aspectRatio="wide"
+									image="https://placeimg.com/800/450/nature"
+								/>
+								<CardTitle
+									title="Title goes here"
+									subtitle="Subtitle here"
+								/>
+								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+								<CardActions >
+									<Button label="Action 1" />
+									<Button label="Action 2" />
+								</CardActions>
+							</Card>
+						</div>
+
+						<div style={{ width: '25%'}}>
+							<Card>
+								<CardTitle
+									avatar="https://placeimg.com/80/80/animals"
+									title="Avatar style title"
+									subtitle="Subtitle here"
+								/>
+								<CardMedia
+									aspectRatio="wide"
+									image="https://placeimg.com/800/450/nature"
+								/>
+								<CardTitle
+									title="Title goes here"
+									subtitle="Subtitle here"
+								/>
+								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+								<CardActions >
+									<Button label="Action 1" />
+									<Button label="Action 2" />
+								</CardActions>
+							</Card>
+						</div>
+
+						<div style={{ width: '25%'}}>
+							<Card>
+								<CardTitle
+									avatar="https://placeimg.com/80/80/animals"
+									title="Avatar style title"
+									subtitle="Subtitle here"
+								/>
+								<CardMedia
+									aspectRatio="wide"
+									image="https://placeimg.com/800/450/nature"
+								/>
+								<CardTitle
+									title="Title goes here"
+									subtitle="Subtitle here"
+								/>
+								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+								<CardActions >
+									<Button label="Action 1" />
+									<Button label="Action 2" />
+								</CardActions>
+							</Card>
+						</div>
+						<div style={{ width: '25%'}}>
+							<Card>
+								<CardTitle
+									avatar="https://placeimg.com/80/80/animals"
+									title="Avatar style title"
+									subtitle="Subtitle here"
+								/>
+								<CardMedia
+									aspectRatio="wide"
+									image="https://placeimg.com/800/450/nature"
+								/>
+								<CardTitle
+									title="Title goes here"
+									subtitle="Subtitle here"
+								/>
+								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+								<CardActions >
+									<Button label="Action 1" />
+									<Button label="Action 2" />
+								</CardActions>
+							</Card>
+						</div>
+						<div style={{ width: '25%'}}>
+							<Card>
+								<CardTitle
+									avatar="https://placeimg.com/80/80/animals"
+									title="Avatar style title"
+									subtitle="Subtitle here"
+								/>
+								<CardMedia
+									aspectRatio="wide"
+									image="https://placeimg.com/800/450/nature"
+								/>
+								<CardTitle
+									title="Title goes here"
+									subtitle="Subtitle here"
+								/>
+								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+								<CardActions >
+									<Button label="Action 1" />
+									<Button label="Action 2" />
+								</CardActions>
+							</Card>
+						</div>
+						<div style={{ width: '25%'}}>
+							<Card>
+								<CardTitle
+									avatar="https://placeimg.com/80/80/animals"
+									title="Avatar style title"
+									subtitle="Subtitle here"
+								/>
+								<CardMedia
+									aspectRatio="wide"
+									image="https://placeimg.com/800/450/nature"
+								/>
+								<CardTitle
+									title="Title goes here"
+									subtitle="Subtitle here"
+								/>
+								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+								<CardActions >
+									<Button label="Action 1" />
+									<Button label="Action 2" />
+								</CardActions>
+							</Card>
+						</div>
+					</Panel>
+				</Layout>
 			</div>
 		)
 	}
