a1c44f6c7fc093950338d6028caf0a8365691138
Земцева Виктория <jlgfmhldf@gmail.com>
2017-07-07 08:41:34 +0300

    feat smoothiescard & more small changes

---
+++ app/components/SmoothiesCard/SmoothiesCard.jsx
@@ -0,0 +1,59 @@
+import React, { PureComponent } from 'react'
+import { string, func } from 'prop-types'
+import noop from 'noop3'
+import {
+	Card,
+	CardMedia,
+	CardTitle,
+	CardText,
+	CardActions,
+	Button,
+} from 'react-toolbox'
+import './SmoothiesCard.css'
+
+export default class SmoothiesCard extends PureComponent {
+	static propTypes = {
+		title: string,
+		subtitle: string,
+		image: string,
+		description: string,
+		onView: func,
+	}
+
+	static defaultProps = {
+		onView: noop,
+	}
+
+	render() {
+		const {
+			title,
+			subtitle,
+			image,
+			description,
+			onView,
+		} = this.props
+		return (
+			<Card>
+				<CardTitle
+					title={title}
+					subtitle={subtitle}
+				/>
+				<CardMedia
+					aspectRatio="wide"
+					image={image}
+				/>
+				<CardText>{description}</CardText>
+				<Button
+					label="Выбрать"
+					onClick={onView}
+					raised
+					accent
+					styles={{
+						width: '100%',
+						height: 60,
+					}}
+				/>
+			</Card>
+		)
+	}
+}
--- app/containers/App/App.jsx
+++ app/containers/App/App.jsx
@@ -12,6 +12,31 @@ import {
 	CardText,
 	CardActions,
 } from 'react-toolbox'
+import SmoothiesCard from '../../components/SmoothiesCard'
+import list from '../../../data/list.json'
+
+const getIngridients = () => {
+	let arrayOfIngridients = []
+
+	list.forEach(item => {
+		const ingridients = Object.keys(item.ingridients)
+
+		Array.prototype.push.apply(arrayOfIngridients, ingridients)
+	})
+
+	return arrayOfIngridients
+}
+
+const renderListOfIngridients = (list, onClick) =>
+	list.map((item, key) =>
+		<Checkbox
+			label={item}
+			onChange={onClick}
+			key={key}
+		/>
+	)
+
+const ingridients = getIngridients(list)
 
 /* eslint-disable no-unused-vars */
 const mapStateToProps = state => ({
@@ -25,6 +50,8 @@ const mapDispatchToProps = dispatch => ({
 @connect(mapStateToProps, mapDispatchToProps)
 export default class App extends PureComponent {
 	render() {
+		console.log(this.props)
+
 		return (
 			<div>
 				<Layout>
@@ -34,194 +61,20 @@ export default class App extends PureComponent {
 						<p>
 							Выберите продукты, которые есть у вас в холодильнике:
 						</p>
-						<Checkbox label="Бананы"/>
-						<Checkbox label="Малина"/>
-						<Checkbox label="Клубника"/>
-						<Checkbox label="Бананы"/>
+						{renderListOfIngridients(ingridients)}
 
 						<Button label='Далее' raised primary/>
 						<h2>
 							Результаты поиска:
+
 						</h2>
 						<div style={{ width: '25%'}}>
-							<Card>
-								<CardTitle
-									avatar="https://placeimg.com/80/80/animals"
-									title="Avatar style title"
-									subtitle="Subtitle here"
-								/>
-								<CardMedia
-									aspectRatio="wide"
-									image="https://placeimg.com/800/450/nature"
-								/>
-								<CardTitle
-									title="Title goes here"
-									subtitle="Subtitle here"
-								/>
-								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
-								<CardActions >
-									<Button label="Action 1" />
-									<Button label="Action 2" />
-								</CardActions>
-							</Card>
-						</div>
-
-						<div style={{ width: '25%'}}>
-							<Card>
-								<CardTitle
-									avatar="https://placeimg.com/80/80/animals"
-									title="Avatar style title"
-									subtitle="Subtitle here"
-								/>
-								<CardMedia
-									aspectRatio="wide"
-									image="https://placeimg.com/800/450/nature"
-								/>
-								<CardTitle
-									title="Title goes here"
-									subtitle="Subtitle here"
-								/>
-								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
-								<CardActions >
-									<Button label="Action 1" />
-									<Button label="Action 2" />
-								</CardActions>
-							</Card>
-						</div>
-
-						<div style={{ width: '25%'}}>
-							<Card>
-								<CardTitle
-									avatar="https://placeimg.com/80/80/animals"
-									title="Avatar style title"
-									subtitle="Subtitle here"
-								/>
-								<CardMedia
-									aspectRatio="wide"
-									image="https://placeimg.com/800/450/nature"
-								/>
-								<CardTitle
-									title="Title goes here"
-									subtitle="Subtitle here"
-								/>
-								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
-								<CardActions >
-									<Button label="Action 1" />
-									<Button label="Action 2" />
-								</CardActions>
-							</Card>
-						</div>
-
-						<div style={{ width: '25%'}}>
-							<Card>
-								<CardTitle
-									avatar="https://placeimg.com/80/80/animals"
-									title="Avatar style title"
-									subtitle="Subtitle here"
-								/>
-								<CardMedia
-									aspectRatio="wide"
-									image="https://placeimg.com/800/450/nature"
-								/>
-								<CardTitle
-									title="Title goes here"
-									subtitle="Subtitle here"
-								/>
-								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
-								<CardActions >
-									<Button label="Action 1" />
-									<Button label="Action 2" />
-								</CardActions>
-							</Card>
-						</div>
-
-						<div style={{ width: '25%'}}>
-							<Card>
-								<CardTitle
-									avatar="https://placeimg.com/80/80/animals"
-									title="Avatar style title"
-									subtitle="Subtitle here"
-								/>
-								<CardMedia
-									aspectRatio="wide"
-									image="https://placeimg.com/800/450/nature"
-								/>
-								<CardTitle
-									title="Title goes here"
-									subtitle="Subtitle here"
-								/>
-								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
-								<CardActions >
-									<Button label="Action 1" />
-									<Button label="Action 2" />
-								</CardActions>
-							</Card>
-						</div>
-						<div style={{ width: '25%'}}>
-							<Card>
-								<CardTitle
-									avatar="https://placeimg.com/80/80/animals"
-									title="Avatar style title"
-									subtitle="Subtitle here"
-								/>
-								<CardMedia
-									aspectRatio="wide"
-									image="https://placeimg.com/800/450/nature"
-								/>
-								<CardTitle
-									title="Title goes here"
-									subtitle="Subtitle here"
-								/>
-								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
-								<CardActions >
-									<Button label="Action 1" />
-									<Button label="Action 2" />
-								</CardActions>
-							</Card>
-						</div>
-						<div style={{ width: '25%'}}>
-							<Card>
-								<CardTitle
-									avatar="https://placeimg.com/80/80/animals"
-									title="Avatar style title"
-									subtitle="Subtitle here"
-								/>
-								<CardMedia
-									aspectRatio="wide"
-									image="https://placeimg.com/800/450/nature"
-								/>
-								<CardTitle
-									title="Title goes here"
-									subtitle="Subtitle here"
-								/>
-								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
-								<CardActions >
-									<Button label="Action 1" />
-									<Button label="Action 2" />
-								</CardActions>
-							</Card>
-						</div>
-						<div style={{ width: '25%'}}>
-							<Card>
-								<CardTitle
-									avatar="https://placeimg.com/80/80/animals"
-									title="Avatar style title"
-									subtitle="Subtitle here"
-								/>
-								<CardMedia
-									aspectRatio="wide"
-									image="https://placeimg.com/800/450/nature"
-								/>
-								<CardTitle
-									title="Title goes here"
-									subtitle="Subtitle here"
-								/>
-								<CardText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
-								<CardActions >
-									<Button label="Action 1" />
-									<Button label="Action 2" />
-								</CardActions>
-							</Card>
+							<SmoothiesCard
+								title='Тест'
+								subtitle='Подзаголовок лалала'
+								image='https://placeimg.com/800/450/nature'
+								description={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type `}
+							/>
 						</div>
 					</Panel>
 				</Layout>
