4232038c1b0e2fd4a061391a7dfa7d0502844105
Земцева Виктория <jlgfmhldf@gmail.com>
2017-07-19 08:23:06 +0300

    fix(storybook): add css modules

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
             test: /\.css$/,
             use: [
                 'style-loader',
-                'css-loader',
+                'css-loader?modules',
                 'postcss-loader',
             ]
         },
