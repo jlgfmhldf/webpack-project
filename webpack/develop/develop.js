26dc3c05bcd11dc766d26cf09c97f2c1c73cbe52
Земцева Виктория <jlgfmhldf@gmail.com>
2017-07-29 23:21:51 +0300

    gzip compression

--- webpack/develop/develop.js
+++ webpack/develop/develop.js
@@ -1,4 +1,5 @@
 import webpack from 'webpack'
+import CompressionPlugin from 'compression-webpack-plugin'
 import baseConfig from '../base'
 
 export default {
@@ -15,5 +16,11 @@ export default {
 	plugins: [
 		...baseConfig.plugins,
 		new webpack.HotModuleReplacementPlugin(),
+		new CompressionPlugin({
+			algorithm: "gzip",
+			test: /\.js$|\.html$/,
+			threshold: 10240,
+			minRatio: 0.8
+		}),
 	],
 }
