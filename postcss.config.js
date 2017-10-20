f44be2298a5c40eeac99339fa887e914cea70f4b
Земцева Виктория <jlgfmhldf@gmail.com>
2017-10-20 17:47:50 +0300

    feat(postcss.config): update

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,23 @@
+const cssnano = require('cssnano')
+
 module.exports = () => ({
 	plugins: [
+		require("postcss-import"),
 		require('postcss-normalize')({
 			forceImport: true
 		}),
 		require('precss'),
 		require('postcss-custom-properties'),
+		require('postcss-size'),
+		require('postcss-font-magician')({
+			hosted: ['./static/fonts',]
+		}),
+		require("postcss-custom-media")(),
+		require('postcss-axis'),
+		require('css-mqpacker'),
+		cssnano({
+			preset: 'default'
+		}),
 		require('autoprefixer'),
 	]
 })
