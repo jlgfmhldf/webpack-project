c9b33af3b048358d5042fee580035b63e3531e82
Земцева Виктория <jlgfmhldf@gmail.com>
2017-10-20 17:21:57 +0300

    feat(App): update

--- app/containers/App/App.jsx
+++ app/containers/App/App.jsx
@@ -2,9 +2,10 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import Test from '../../components/Test'
 
-import {
-	testAction,
-} from '../../actions/index'
+import s from './App.pcss'
+
+// import {
+// } from '../../actions/index'
 
 /* eslint-disable no-unused-vars */
 const mapStateToProps = state => ({
@@ -12,21 +13,18 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-	testAction: () => dispatch(testAction()),
+	// testAction: () => dispatch(testAction()),
 })
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class App extends PureComponent {
-	componentWillMount() {
-		this.props.testAction()
-	}
 
 	render() {
 		return (
-			<div>
+			<div className={s.App}>
 				<Test>
-					Hello!!!
-				</Test>
+					Webpack Starter
+			</Test>
 			</div>
 		)
 	}
